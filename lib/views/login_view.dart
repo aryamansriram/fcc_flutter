8492bd6b213efdf2e59df07c4df8affca8bc5ace
aryamansriram <aryaman.sriram@gmail.com>
2022-03-16 20:00:02 +0530

    Added notesview route and navigation

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fcc_app/firebase_options.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({ Key? key }) : super(key: key);
@@ -68,16 +69,18 @@ class _LoginViewState extends State<LoginView> {
                         email: email, 
                         password: password
                         );
-                        print(userCredential);
+                        //devtools.log(userCredential.toString());
+                        Navigator.of(context)
+                          .pushNamedAndRemoveUntil('/notes', (route) => false,);
                       
     
                       }
                       on FirebaseAuthException catch(e){
                           if (e.code=='user-not-found'){
-                            print('User not found');
+                            devtools.log('User not found');
                           }
                           else if(e.code=='wrong-password') {
-                            print("wrong password");
+                            devtools.log("wrong password");
                           }
                       }
                       
