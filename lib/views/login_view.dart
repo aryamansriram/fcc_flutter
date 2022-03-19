ce15accf59cd13cc9e5072817308dd801eb4c617
aryamansriram <aryaman.sriram@gmail.com>
2022-03-19 12:41:03 +0530

    Added error handling to Login view

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:fcc_app/utilities/show_error_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -78,10 +79,18 @@ class _LoginViewState extends State<LoginView> {
                       on FirebaseAuthException catch(e){
                           if (e.code=='user-not-found'){
                             devtools.log('User not found');
+                            await showErrorDialog(context, 'User not found');
                           }
                           else if(e.code=='wrong-password') {
                             devtools.log("wrong password");
+                            await showErrorDialog(context, 'Wrong Password');
                           }
+                          else {
+                            await showErrorDialog(context, 'Error ${e.code}');
+                          }
+                      }
+                      catch(e){
+                        await showErrorDialog(context, e.toString());
                       }
                       
                       //print("Email: $email");
